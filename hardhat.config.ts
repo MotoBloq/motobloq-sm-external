4609638df6d1bb190b081b5c34d78b80fc3a6a9e
Patryk Matuszewski <patryk@matuszewski.it>
2023-06-05 15:33:23 +0200

    - New Sepolia deployment - Add deployment script supporting KMS

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,6 @@
 import 'hardhat-deploy';
 import '@nomicfoundation/hardhat-toolbox';
+import "@rumblefishdev/hardhat-kms-signer";
 import {HardhatUserConfig} from "hardhat/config";
 import {nodeUrl, accounts} from './utils/network';
 
@@ -17,7 +18,7 @@ const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
   namedAccounts: {
     deployer: 0,
-    royaltyReceiver: 1,
+    royaltyReceiver: 0,
   },
   networks: {
     hardhat: {},
@@ -27,7 +28,8 @@ const config: HardhatUserConfig = {
     },
     sepolia: {
       url: nodeUrl('sepolia'),
-      accounts: accounts('sepolia'),
+      // accounts: accounts('sepolia'),
+      kmsKeyId: "9026f063-b969-4279-93b4-0d7a006bf6b6"
     },
     goerli: {
       url: nodeUrl('goerli'),
